f9f8d328cd6b30002edb18cd5140f924c4dc18d5
panzehir19 <41573243+panzehir19@users.noreply.github.com>
2021-04-22 13:19:14 +0300

    body ss

---
+++ app.py
@@ -0,0 +1,19 @@
+#coding=utf-8                                                                                                                                                                              
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+options = webdriver.ChromeOptions()
+options.headless = True
+driver = webdriver.Chrome(options=options)
+
+URL = 'https://eksim.com.tr'
+
+driver.get(URL)
+
+S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
+driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment                                                                                                                
+driver.find_element_by_tag_name('body').screenshot('web_screenshot.png')
+
+driver.quit()
