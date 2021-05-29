5209ffe87d99830b5325ba20f5f382819bb55066
panzehir19 <41573243+panzehir19@users.noreply.github.com>
2021-05-29 18:53:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-from typing import Pattern
 from selenium import webdriver
 import time
 import json
 import re
+from PIL import Image
+from pathlib import Path
 
 driver = webdriver.Chrome()
-pattern = 'https:\/\/(.*).com.tr'
 
 with open('site_list.json', "r") as json_file:
     data = json.load(json_file)
 
+pattern = r'https:\/\/(.*)\.com\.tr'
+
+cwd = Path.cwd()
+mod_path = Path(__file__).parent
+rel_path = 'yeni/'
+
 for site in data["site"]:
-    #driver.maximize_window()
+    driver.maximize_window()
     driver.get(site)
     driver.refresh()
     print (site)
     print(type(site))
     time.sleep(3)
-    filename = re.match(pattern, site)
+    filename = re.search(pattern, site)
+    print (filename.group(1))
     print (filename)
-    driver.save_screenshot(filename)
-
-
+    driver.save_screenshot((filename.group(1) + '.png'))
 
 driver.close()
