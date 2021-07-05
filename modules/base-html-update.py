25ded8ce451397ff02f389eb1628e11e59655c7a
Alp <49457057+alpkeskin@users.noreply.github.com>
2021-07-05 11:46:34 +0300

    Update base-html-update.py

--- modules/base-html-update.py
+++ modules/base-html-update.py
@@ -1,37 +1,26 @@
-from requests_html import HTMLSession
-import json
-import requests
+# LIBS
+import requests,os
 from bs4 import BeautifulSoup
-import re
-import os
-import configparser
 
-parser = configparser.ConfigParser()
-parser.read("config.txt")
+# VARIABLE TO BE TAKEN
+site = "https://tedu.edu.tr" 
 
-session = HTMLSession()
+# 4 FILE WITHOUT ':'
+x = site.index(":")
+domain = site[x+3:]
 
-pattern = r'https:\/\/(.*)\.com\.tr'
+# FILE NAME
+base_html = domain+"_base.txt"
 
-dir = parser.get("directory", "dir")
-based_html_dir = parser.get("directory", "based_html_dir")
+def update_base(site):
+	if os.path.exists(base_html):
+		response = requests.get(site)
+		soup = BeautifulSoup(response.content, 'html.parser')
+		f = open(base_html, "w")
+		f.write(str(soup.prettify()))
+		f.close()
+	else:
+		#error report
+		print("base html file does not exist!")
 
-with open('site_list.json', "r") as json_file:
-    data = json.load(json_file)
-
-for site in data["site"]:
-    r = session.get(site)
-    # req = requests.get(site)
-    soup = BeautifulSoup(r.text, features="lxml")    
-    full_html = soup.prettify()
-    filename = re.search(pattern, site)
-    text_file = open(f"{filename.group(1)}_baseHTML.txt", "w" , encoding="utf-8")
-    n = text_file.write(full_html)
-    text_file.close()
-    # os.replace(f'{dir}{filename.group(1)}_baseHTML.txt', f'{dir}{based_html_dir}')
-    os.replace(f'{dir}{filename.group(1)}_baseHTML.txt', f'{dir}{based_html_dir}/{filename.group(1)}_baseHTML.txt')
-    print(f'{filename.group(1)} base image tasindi')
-
-
-
-
+update_base(site)
